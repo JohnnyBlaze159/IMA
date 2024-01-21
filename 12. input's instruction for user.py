940bb3d96b5922cb20bc5f25ae903e755720577b
JohnnyBlaze159 <156817764+JohnnyBlaze159@users.noreply.github.com>
2024-01-21 02:05:27 +0000

    Add files via upload

---
+++ 12. input's instruction for user.py
@@ -0,0 +1,8 @@
+print("How old are you?", end = ' ')
+age = input("age?")
+print("How tall are you?", end = ' ')
+height = input("height?")
+print("How much do u weigh?", end = ' ')
+weight = input("weight?")
+
+print (f"So you are {age} years old, my height is {height} and my weight is {weight}.")
